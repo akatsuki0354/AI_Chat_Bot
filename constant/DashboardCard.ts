35aa13761d82cf7f2d5d102b0ae35119a1fdcdfa
Franco Gregorio <francogregorio2004@gmail.com>
2025-10-17 07:09:03 +0800

    feat: implement dynamic card rendering using CardComponents for improved scalability

---
+++ constant/DashboardCard.ts
@@ -0,0 +1,17 @@
+export const CardComponents = [{
+    title: "Total Chats",
+    id: '1',
+},
+{
+    title: "Chat Total Token",
+    id: '2'
+},
+{
+    title: "Respond Token",
+    id: '3'
+},
+{
+    title: "Total Token Used",
+    id: '4'
+},
+]
