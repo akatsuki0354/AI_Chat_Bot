429fa230a20c8951ec1afb09cb9b96a7a40fb53e
Franco Gregorio <francogregorio2004@gmail.com>
2025-10-17 04:10:40 +0800

    refactor(page): simplify authentication handling by removing loading state and unnecessary comments in the page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 "use client";
 import LoginPage from "./SignIn/SignIn";
 import { useAuth } from "@/services/Auth";
-import Loading from "@/components/loading";
+
 import Home from "./Home/page";
 export default function Page() {
-  const { user, loading } = useAuth();
-
-  // Show loading state while checking authentication
-  if (window !== undefined && loading) {
-    return <Loading />;
-  }
+  const { user } = useAuth();
 
   return (
     <div >
       {user ? (
         <div>
-          {/* <p>Your email: {user.email}</p>
-          <Button onClick={handleSignOut}>SignOut</Button> 
-          */}
           <Home />
         </div>
       ) : (
