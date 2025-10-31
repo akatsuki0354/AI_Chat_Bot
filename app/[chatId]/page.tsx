9bfb9ca589d2cb522759f6f4660e2cbd2518b5a9
Franco Gregorio <francogregorio2004@gmail.com>
2025-10-31 18:26:55 +0800

    feat: enhance message input with updated placeholder, improved styling, and add send button

--- app/[chatId]/page.tsx
+++ app/[chatId]/page.tsx
@@ -7,7 +7,7 @@ import ReactMarkdown from "react-markdown";
 import { timeAgo } from "@/utils";
 import React, { useEffect, useState, useRef } from "react";
 import { useParams, useRouter } from "next/navigation";
-
+import { Send } from "lucide-react";
 function Page() {
   const { addChat, setCurrentConvoId } = useChatStore();
   const params = useParams();
@@ -101,9 +101,9 @@ function Page() {
                 </div>
               </div>
             )}
-            <div className="flex gap-2 mt-5">
+            <div className="relative border-1 rounded-md shadow-sm">
               <Textarea
-                placeholder="Ask Anything.."
+                placeholder="Ask Another Question.."
                 value={message}
                 rows={1}
                 onChange={(e) => setMessage(e.target.value)}
@@ -114,25 +114,31 @@ function Page() {
                   }
                 }}
                 className="
-  chats
-    md:!text-[17px] 
-    md:placeholder:text-lg 
-    resize-none 
-    overflow-hidden 
-    border 
-    border-gray-300 
-    rounded-md 
-    focus:!outline-none 
-    focus:!ring-0 
-    focus:!border-gray-300 
-    leading-tight 
-    pt-5
-    max-h-[6rem]
-    overflow-y-auto
-    pr-24
-  "
+                chats
+                md:!text-[17px] 
+                md:placeholder:text-lg 
+                resize-none 
+                overflow-hidden 
+                shadow-none
+                border-0
+                rounded-md 
+                focus:!outline-none 
+                focus:!ring-0 
+                focus:!border-0
+                leading-tight 
+                pt-5
+                max-h-[24rem]
+                overflow-y-auto
+                mb-3
+ "
               />
-
+              <div className="mb-14">
+                <Button
+                  className=" absolute mr-2 right-0 mb-0 "
+                >
+                  <Send className="text-white" />
+                </Button>
+              </div>
             </div>
           </div>
         </form>
